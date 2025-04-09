a395a114268801130c05a1cdc250a7959b38a076
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-09 17:52:22 +0530

    Operators

---
+++ Datatype and Operators/Datatype.js
@@ -0,0 +1,69 @@
+// Comments : 
+// single Line //
+/*
+    Multiline Comment
+*/
+
+
+// number 
+// string 
+// BigInt 
+// Boolean 
+// Object 
+// Function
+// undefined
+
+// console.log("Connected ");
+
+// Number = // int float double 
+// console.log(12);
+// console.log(12.34);
+// console.log(12.1234567890987);
+
+//String = char /string
+// console.log('Zafar');
+// console.log("Zafar");
+
+
+// How to Check Datatype : 
+// console.log(typeof(12));
+// console.log(typeof(12.34));
+// console.log(typeof(12.1234567));
+
+
+// console.log(typeof 12);//valid
+// console.log(typeof "Zafar");
+// console.log(typeof 'Zafar');
+
+// console.log(1234567890987654321);
+
+
+//BigInt : 
+// console.log(1234567890987654321n);
+// console.log(typeof 1234567890987654321n);
+
+
+//Boolean : 
+// console.log(true);
+// console.log(false);
+// console.log(typeof false);
+
+// console.log(function(){});
+// console.log(typeof function(){});
+
+
+// console.log(undefined);
+// console.log(typeof undefined);
+
+
+// Object : 
+
+// console.log([11,12,13,14]);
+// console.log(typeof [11,12,13,14]);
+// console.log(Array.isArray( [11,12,13,14]));
+
+// console.log({"name" : "Zafar"});
+// console.log(typeof {"name" : "Zafar"});
+
+// console.log(null);
+// console.log(typeof null);
---
+++ Datatype and Operators/Operators.js
@@ -0,0 +1,71 @@
+// Operators : 
+// Arithmatic  : + - * / % 
+// unary  :  ++ , --
+// logical : && , ||  , !(NOT)
+
+// bitwise  : & | ^ ~  : 
+
+// assignment : +=,-=,*=,/=,%=
+// conditional/relational : <,>,<=,>=,==,!=,==,===,!==
+// typeof operator
+
+
+//Arithmatic Op :
+// number :  addition 
+// console.log(12+13);
+// String :  concatination 
+// console.log("Zafar"+"Shabhai");
+
+// 1st number : 2nd String(char)   concatination 
+// console.log(12+"Zafar");
+
+// 1st number : 2nd String(numeric)    concatination 
+// console.log(12+"13");
+
+// console.log(12-"Zafar");  //NaN
+// console.log(12-"13");
+
+
+
+// console.log(12<13 && 23>10); //true
+
+// console.log(12<13 && "Zafar"); // Zafar
+
+// console.log(12 && 13); // 13 
+
+// console.log(12<13 && ""); // 
+// console.log(12>13 && "Zafar"); // false
+
+// console.log(0 && "Zafar"); 
+
+// console.log(0<12 && 0);   // 
+
+
+// 0   0   0
+// 1   0   0
+// 0   1   0
+// 1   1   1
+
+// console.log(12<13 || "Zafar"); //true
+// console.log(12<13 ||0); //true
+// console.log(12>13 || "Zafar"); //Zafar
+
+// console.log(12 || 13); // 12
+
+// console.log(!(0));
+
+
+
+// console.log(12 == 12);
+// console.log(12 != 12);
+
+// console.log(12 == "12");
+// console.log(12 != "12");
+
+// console.log(12 == "13");
+// console.log(12 != "Z");
+
+
+// first : datatype 
+// console.log(12 === "12");  //type
+// console.log(12 !== "12");  //type
