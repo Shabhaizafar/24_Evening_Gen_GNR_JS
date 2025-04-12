7f636780395492400bbd983f5971ed8044d1faa2
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-12 20:16:29 +0530

    Variable

---
+++ Datatype and Operators/Variable.js
@@ -0,0 +1,61 @@
+// start : not a Numeric
+// variablename :no white space
+// samevariable : No 
+// Keyword : 
+// Special : _ , $ 
+
+
+// How TO Declare a Variable in  a JS : 
+
+//1) var : 
+// var number1 = 12;
+// console.log(number1);
+// console.log(typeof number1);
+
+// var fname = "Zafar";
+// console.log(fname);
+// console.log(typeof fname);
+
+var a = 12;
+console.log(a);
+
+// a=100; //valid
+// console.log(a);
+var a  = "ZZfara";
+console.log(a);
+
+
+
+//2) let : 
+// let number1 = 12;
+// console.log(number1);
+// console.log(typeof number1);
+
+// let fname = "Zafar";
+// console.log(fname);
+// console.log(typeof fname);
+let b = 13;
+console.log(b);
+
+// b=200; //valid
+// console.log(b);
+
+// let b = "Zafar"; //in-valid
+
+
+
+//3) const : 
+// const number1 = 12;
+// console.log(number1);
+// console.log(typeof number1);
+
+// const fname = "Zafar";
+// console.log(fname);
+// console.log(typeof fname);
+const c = 15;
+console.log(c);
+
+// c = 300;   //invalid
+// console.log(c);
+
+// const c = 1222;
