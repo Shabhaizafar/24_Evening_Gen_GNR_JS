794482810565a4d834d3d1e134e9ef0e72feffb5
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-14 18:02:06 +0530

    Inbuilt Function

---
+++ In-Built Function/function1.js
@@ -0,0 +1,49 @@
+// Function :- 
+// 1. In-Built Functiion  :
+// prompt : take an input    : default datatype : string
+
+// var a = +prompt("Enter the Value of A : ");   // 12   12,13
+// var b = +prompt("Enter the Value of B : ");  // "Z"  90   //true //1
+// // var a = Number(prompt("Enter the Value of A : "));
+// // var b = Number(prompt("Enter the Value of B : "));
+// console.log("Value of A : ",a);
+// console.log("Value of B : ",b);
+
+// console.log("Addition Of A and B : ",a+b); // 30
+
+
+
+// var a = +prompt("Enter the Value of A : ");  // true
+// var b = +Boolean(prompt("Enter the Value of B : "));   // 14
+
+// console.log("Value of A : ",a);
+// console.log("Value of B : ",b);
+
+// console.log("Addition Of A and B : ",a+b); // 30
+
+
+// var a = +prompt("Enter the Value of A : ");  // 15
+// var b = +Boolean(prompt("Enter the Value of B : "));   // true
+
+// console.log("Value of A : ",a);
+// console.log("Value of B : ",b);
+
+// console.log("Addition Of A and B : ",a+b); // 30
+
+//-----------------------------------------------------------------
+// alert : msg display as pop's
+
+// alert("Hello");
+
+// confirm :  user-confirmation.
+
+// var ans = confirm("Are you Ready ?");
+// console.log(ans);
+
+
+
+// 2. User-defined Function 
+
+// true + 12 = 13
+// 'true' + 12 = NaN
+
