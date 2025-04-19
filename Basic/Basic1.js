2b350abe388f39e308371ad1d92b731317bcd854
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-19 17:57:10 +0530

    Basic Continue

---
+++ Basic/Basic1.js
@@ -0,0 +1,100 @@
+// Conditional Control Statement : 
+// 1. if  statement.
+// if (condition) {
+//     code;
+// }
+// 2. if-else  statement.
+// if (condition) {
+//     code;
+// }else{
+//     code2;
+// }
+// 3. else-if statement.
+// if (condition) {
+//     code;
+// }else if(condition2){
+//     code3;
+// }
+// else{
+//     code2;
+// }
+// 4. nested if-else  statement.
+// if (condition) {
+//     code;
+// }else{
+//     if (condition) {
+//         code3;
+//     }else{
+//         code2;
+//     }
+// }
+    // - ternary Statement/ Ternary Op : 
+    // (condition) ? code1 : code2;
+
+// case Control Statement : 
+// 1. switch case :
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+// var a = 1;
+// switch (a) {
+//     // case 1: console.log("Case 1");
+//     //     break;
+//     case +(12<13) : console.log("Case 1");
+//             break;
+//     case 2: console.log("Case 2");
+//         break;
+//     case 'Zafar' : console.log("String Case ");
+//         break;
+//     default:console.log("Default");
+//         break;
+// }
+
+// 2. nested-switch case
+
+// Loop Control Statement : 
+//  1. for loop : 
+
+// var i;
+// for ( i = 0; i <10; i++) {
+//     //code
+// }
+// for (let i = 0; i <10; i++) {
+//     //code
+// }
+//var i=1;
+// for(;condition;){
+
+//     //inc/dec
+// }
+
+//  2. while Loop : 
+
+// initialization
+// while (condition) {
+//     //inc/dec
+// }
+
+//  3. do-while Loop :-
+//initialization 
+// do {
+    // inc/dec
+// } while (condition);
+
+
+
+//for-each loop 
+//for-in loop
+//for-of loop
+
+
+// * 
+// * * 
+// * * *
+// * * * *
+
