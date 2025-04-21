1d988595d017249c9f515ceba9be9609a53a18ba
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-21 17:53:52 +0530

    String,Array

---
+++ Array/Array1.js
@@ -0,0 +1,61 @@
+//=> Array : 
+//-> Collection of Data with same or Different Data type.
+
+// same datatype
+// var arr1 = [1,2,3,4];
+// var arr2 = ["Raj","Shah"];
+// var arr3 = [true,false];
+// console.log(arr1);
+// console.log(arr2);
+// console.log(arr3);
+
+// different Datatype  
+// var arr4 = ["Raj",12,true,null,undefined,"",{}];
+// console.log(arr4);
+
+
+// index :
+// var arr = [11,12,13,14];
+// //          0  1  2  3
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[10]); // undefined
+
+// length : property
+// console.log(arr.length);
+
+
+// Basic Methods : 
+// 1. push :  add new Value at a last index 
+// return a new length
+// var ans  = arr.push("Zafar","Shah");
+// console.log(arr);
+// console.log(ans);
+
+// 2. pop :  remove :last index 
+// return deleted Value
+// var ans2 = arr.pop();
+// console.log(arr);
+// console.log(ans2);
+
+// 3. shift : remove : starting index
+// var ans3 = arr.shift();
+// return deleted Value
+// console.log(arr);
+// console.log(ans3);
+
+// 4. unshift :   add new Value : starting index
+// return a new Index
+// var ans4 = arr.unshift(100,102);
+// console.log(arr);
+// console.log(ans4);
+
+
+
+
+// const arr = [11,12,13];
+// arr.pop();
+// console.log(arr);
+
+
+// reference Datatype : 
