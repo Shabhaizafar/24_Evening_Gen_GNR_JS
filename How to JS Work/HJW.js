c56bd3f84002c8beae1f15699064a5872920841e
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-05 18:03:28 +0530

    HJW

---
+++ How to JS Work/HJW.js
@@ -0,0 +1,73 @@
+// Main();
+// console.log("Outer 1 A : ",a); //30
+// var a = 10;
+// function Main(){
+//      a = 30;
+//     console.log("Main A : ",a); //30
+// }
+// console.log("Outer 2 A : ",a); //10
+
+
+
+// let a = 10;
+// var b = 30;
+// const c = 50;
+
+// function Main(){
+//     var myfu = function(){
+//         console.log("Main1 A : ",a); 
+//         console.log("Main1 B : ",b); 
+//         console.log("Main1 C : ",c); 
+//         a = 100;
+//         var b = 200;
+//         var c = 300;
+//         console.log("Inner A : ",a); 
+//         console.log("Inner B : ",b); 
+//         console.log("Inner C : ",c); 
+//     }
+//     myfu();
+//     console.log("Main2 A : ",a); 
+//     console.log("Main2 B : ",b); 
+//     console.log("Main2 C : ",c); 
+// }
+// console.log("Outer1 A : ",a); 
+// console.log("Outer1 B : ",b); 
+// console.log("Outer1 C : ",c); 
+// Main();
+
+// console.log("Outer2 A : ",a); 
+// console.log("Outer2 B : ",b); 
+// console.log("Outer2 C : ",c); 
+
+
+
+
+Main2();
+var a = 10;
+let b = 20;
+const c = 30;
+console.log("Outer1 A1",a);
+console.log("Outer1 B1",b);
+console.log("Outer1 C1",c);
+function Main1(){
+    console.log("Main1 A1",a);
+    var a = 100;
+    let b = 200;
+    const c = 300;
+    console.log("Main1 A2",a);
+    console.log("Main1 B1",b);
+    console.log("Main1 C1",c);
+}
+
+Main1();
+console.log("Outer2 A1",a);
+console.log("Outer2 B1",b);
+console.log("Outer2 C1",c);
+function Main2(){
+    a = 100;
+    b = 200;
+    c = 300;
+    console.log("Main2 A1",a);
+    console.log("Main2 B1",b);
+    console.log("Main2 C1",c);
+}
---
+++ Scope/scope2.js
@@ -0,0 +1,57 @@
+// global Scope : 
+// var a = 12;
+// let b = 13;
+// const c = 14;
+// var a = 12;
+
+// console.log(a);
+// for (let i = 0; i <1; i++) {
+//     var a = 13;
+//     console.log(a);    
+// }
+// console.log(a);
+
+
+
+// let a = 12;
+// console.log(a); //12
+// for (let i = 0; i <1; i++) {
+//     let a = 13;
+//     console.log(a);  //13   
+// }
+// console.log(a); //12
+
+
+// const a = 12;
+// console.log(a); //12
+// for (let i = 0; i <1; i++) {
+//     var a = 13;
+//     console.log(a);  //13   
+// }
+// console.log(a); //12
+
+// block Scope :
+// Local Scope / function Scope :
+
+
+
+// const a = 12;
+// console.log(a);
+// function Hello(){
+//     var a = 100;
+//     console.log(a);
+// }
+// Hello();
+// console.log(a);
+
+
+
+
+
+/*
+Global Scope Variable  : var  
+Block Scope Variable : let , const 
+{
+
+}
+*/
