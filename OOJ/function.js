0ddc27964d2b223ff217420339184359b664a5cc
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-13 17:56:48 +0530

    Call,Apply,bind,this,new

---
+++ OOJ/function.js
@@ -0,0 +1,53 @@
+// function sayHello(){
+//     console.log("Hello");
+// };
+
+// sayHello();
+
+
+// sayHello.call();
+// sayHello.apply();
+// var ans = sayHello.bind();
+// ans();
+
+
+
+
+// var person1 = { 
+//     fname : "Raj",
+//     lname : "Shah",
+//     age : 20,
+//     bioData : function (hobby1,hobby2){
+//         console.log(`Your Name is ${this.fname} ${this.lname}.Your age ${this.age}.Your Hobbies : ${hobby1},${hobby2}`);
+//     }
+// }
+
+
+// var person2 = { 
+//     fname : "Ajay",
+//     lname : "Sharma",
+//     age : 23
+// };
+
+
+// console.log(person1);
+// console.log(person2);
+
+
+// person1.bioData();
+
+// person2.bioData();
+
+
+// person1.bioData.call(person2,"Cricket","Football");
+// person1.bioData.call(person1);
+
+
+// person1.bioData.apply(person1,["Cricket","Football"]);
+// person1.bioData.apply(person2);
+
+
+// var ans1 = person1.bioData.bind(person1);
+// var ans2 = person1.bioData.bind(person2);
+// ans1();
+// ans2();
---
+++ OOJ/newThis.js
@@ -0,0 +1,67 @@
+console.log("New and This");
+
+
+// new : empty Object : {}
+
+
+
+// this :  global 
+
+// this : 
+
+
+// C = new Person("wer",:we);
+
+
+
+// function Person(){
+//     this.fname = "Raj";
+//     this.Age = 12;
+//     return this;
+// }
+
+// var p1 = new Person();
+
+
+
+// function Person(fname,age){
+//     // this  = new : {}
+//     this.fname = fname;
+//     this.Age = age;
+
+//     // {fname : "Raj",Age : 13}
+// }
+
+// var p1 = new Person("Raj",13);
+
+
+// console.log(p1);
+
+// function Person(fname,age){
+//     this.fname = fname;
+//     this.Age = age;
+// }
+
+// var p1 = new Person("Raj",13);
+
+
+// console.log(p1);
+
+
+/*
+
+### 1. **Method Borrowing with `call()`**
+
+**Problem:** You have multiple user objects, but only one has the `getFullName` method.
+
+**Use:** Borrow the method using `call()`.
+
+---
+
+### 2. **Using `apply()` to Pass Arguments from an Array**
+
+**Problem:** You need to find the maximum number from a list stored in an array.
+
+
+
+*/
