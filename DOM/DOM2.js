2f72093e89c0f18dee1d183976c195e746eb9ccb
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-20 17:55:52 +0530

    DOM Continue

---
+++ DOM/DOM2.js
@@ -0,0 +1,18 @@
+// How to Access HTML Element Content Using JS : 
+
+// console.log(document.querySelector('div'));
+
+// 1. innerHTML : 
+// console.log(document.querySelector('div').innerHTML);
+
+// 2. innerText : 
+// console.log(document.querySelector('div').innerText);
+
+// 3. textContent : 
+// console.log(document.querySelector('div').textContent);
+
+
+// --------------------------------------------------------------//
+// How to Change/Modify HTML Element Content using JS:  
+
+document.querySelector('h1').innerHTML = "Changed Data";
