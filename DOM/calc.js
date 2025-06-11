afbc71e9ca94bd76e76f55299371d0c80cf793e7
Shabhaizafar <zafarshabhai03@gmail.com>
2025-06-11 18:14:54 +0530

    Calc

--- DOM/calc.js
+++ DOM/calc.js
@@ -5,6 +5,35 @@ const arr = ['%','+',"-",'/','*'];
 calc_body.addEventListener('click',function(event){
     var calc_types = document.querySelector('.calc-typed');
     if(event.target.classList.contains('button')){
+        if(event.target.innerHTML == "="){
+            var ans = "";
+            var n2 = "";
+            var flag = true;
+            var counter = 0;
+            var op = "";
+            for (let i = 0; i < calc_types.innerHTML.length; i++) {
+                if(arr.includes(calc_types.innerHTML[i])){
+                    flag = false;
+                    counter++;
+                    op = calc_types.innerHTML[i];
+                   
+                    continue;
+                }
+                if(flag){
+                    ans+=calc_types.innerHTML[i];
+                }
+                else{
+                    n2+=calc_types.innerHTML[i];
+                }
+   
+            }
+            switch (op) {
+                case '+': console.log(+ans+(+n2));
+                    break;
+            }
+            return;
+        }
+
         if(event.target.innerText>=0 && event.target.innerText<=9){
             if(calc_types.innerHTML==0){
                 calc_types.innerHTML=event.target.innerText;    
