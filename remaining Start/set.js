81bd3a5121da174675103d6f857a060ef9b44c41
Shabhaizafar <zafarshabhai03@gmail.com>
2025-06-23 17:53:21 +0530

    Set

---
+++ remaining Start/set.js
@@ -0,0 +1,68 @@
+var arr = [1,14,34,11,12,11,2,1,1,2,33];
+var mystr = "Royal Techno";
+
+// how to create a Emtpy Set :  
+
+// var myset = new Set();
+
+// console.log(myset);
+
+// console.log(typeof myset);
+
+
+// how to create a Filled Set : 
+// var myset = new Set(arr);
+
+// console.log(myset);
+
+
+// var myset = new Set(mystr);
+
+// console.log(myset);
+
+
+
+
+// var myset = new Set();
+// console.log(myset);
+
+// single
+// myset.add(11); 
+// myset.add(11,12);    only sinlge value applied
+// myset.add(...arr);
+// myset.add("Error");
+// console.log(myset);
+
+
+// myset.clear();
+// myset.delete(11);
+
+
+
+// console.log(myset.entries());
+
+// console.log(myset.keys());
+// console.log(myset.values());
+
+
+// console.log(myset.size);
+
+
+// console.log(myset.has("Royal"));
+// console.log(myset.has(1));
+
+// myset.forEach((element,i)=>{
+//     console.log(element,i);
+// });
+
+
+
+// for (const key in myset) {
+//     console.log(key);
+// }
+
+// for (const key of myset) {
+//     console.log(key);
+// }
+
+
---
+++ remaining Start/useful.js
@@ -0,0 +1,44 @@
+// - Iterables :->
+// Iterables : 
+// Index 
+// all loops are working
+
+var arr = [11,12,13,14,15];
+console.log(arr);
+
+
+
+// var obj = {
+//     1 : "Raj1",
+//     2 : "Raj2",
+//     3 : "Raj3",
+//     4 : "Raj4",
+// }
+
+// for (const key in obj) {
+//     console.log(key);
+// }
+
+// for (const key of obj) {
+//     console.log(key);
+// }
+
+// var str = "Royal tech";
+// for (const key in str) {
+//     console.log(key);
+// }
+
+// for (const key of str) {
+//     console.log(key);
+// }
+
+
+// for (const key in arr) {
+//     console.log(typeof key);
+// }
+
+
+//    - Sets.
+//    - Maps.
+//    - Object.assign.
+//    - Optional chaining.
