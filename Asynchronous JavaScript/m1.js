332e36071e10a67cf9ce1ce861ac72a553517956
Shabhaizafar <zafarshabhai03@gmail.com>
2025-07-29 17:43:15 +0530

    worker

---
+++ Asynchronous JavaScript/m1.js
@@ -0,0 +1,58 @@
+// var id;
+// document.querySelectorAll('button')[0].addEventListener('click',function(){
+//     id = setInterval(() => {
+//         document.querySelector('span').innerText= +(document.querySelector('span').innerText)+1;
+//     }, 1000);
+// });
+// document.querySelectorAll('button')[1].addEventListener('click',function(){
+//     clearInterval(id);
+// });
+
+
+// var worker;
+// document.querySelectorAll('button')[0].addEventListener('click',function(){
+//     if(!worker){
+//         worker =  new Worker('worker2.js');
+//          worker.onmessage = function (e) {
+//             document.querySelector('span').innerText = e.data;
+//         };
+//     }
+// });
+
+
+// document.querySelectorAll('button')[1].addEventListener('click',function(){
+//     if(worker){
+//         worker.terminate();
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Asynchronous JavaScript/worker2.js
@@ -0,0 +1,5 @@
+var i = 0;
+setInterval(() => {
+        postMessage(i);
+        i++;
+}, 1000);
